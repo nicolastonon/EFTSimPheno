31e9f4216d45d47ae8f1f99713f0bd6a5b791fc2
ntonon <nicolas.tonon@desy.de>
2020-12-21 21:30:43 +0100

    update

---
+++ Pheno/Plotter/Utils/LinkDef.h
@@ -0,0 +1,16 @@
+#ifdef __CINT__
+
+#pragma link off all global;
+#pragma link off all class;
+#pragma link off all function;
+// #pragma link C++ nestedclasses;
+// #pragma link C++ nestedtypedef;
+
+#pragma link C++ class WCPoint+;
+#pragma link C++ class WCFit+;
+#pragma link C++ class TH1EFT+;
+
+// #pragma link C++ class std::vector<TH1EFT>+;
+// #pragma link C++ defined_in "TH1EFT.h";
+
+#endif
