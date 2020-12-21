2beac6199cf772a3003091cbc3909e9805dbd3c1
ntonon <nicolas.tonon@desy.de>
2020-12-21 21:36:28 +0100

    update

--- Pheno/Plotter/PlotXsecVsOperator.cc
+++ Pheno/Plotter/PlotXsecVsOperator.cc
@@ -51,11 +51,6 @@
 #include <fstream>
 #include <boost/algorithm/string.hpp>
 
-//Custom classes for EFT (see https://github.com/Andrew42/EFTGenReader/blob/maste)
-#include "/home/ntonon/Postdoc/TopEFT/Analysis/Utils/TH1EFT.h"
-#include "/home/ntonon/Postdoc/TopEFT/Analysis/Utils/WCPoint.h"
-#include "/home/ntonon/Postdoc/TopEFT/Analysis/Utils/WCFit.h"
-
 #include "./Helper.h"
 
 #include <cassert>     //Can be used to terminate program if argument is not true.
